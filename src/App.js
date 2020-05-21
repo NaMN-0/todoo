886a332e73f10117ab0d9a3feed2a9e493ffe56a
NaMN-0 <naman1474@gmail.com>
2020-05-21 18:20:42 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const uuid = require('uuid')
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      msg : "",
+      taskList : []
+    }
+
+  }
+
+  changeMsg = (event) => {
+    this.setState({msg:event.target.value})
+    console.log(this.state.msg)
+  }
+
+  addTask = (event) => {
+    event.preventDefault()
+    if(this.state.msg){
+      const list1 = this.state.taskList
+      const task1 = {
+        id : uuid.v4(),
+        task : this.state.msg
+      }
+      list1.push(task1)
+      this.setState({taskList:list1})  
+    }
+  }
+
+  deleteTask = (id) => {
+    console.log(id)
+    const list1 = this.state.taskList
+    const list2 = list1.filter(task => (task.id)!==(id))
+    this.setState({taskList:list2})
+  }
+
+  render() {
+      return (
+          <div className='App'>
+            <h1 style={head} className="my-3 mx-auto border rounded border-secondary">TO-DO List</h1>
+            <form action="" onSubmit={this.addTask}>
+              <input type="text" name="data" autoComplete="off" onChange={this.changeMsg}/>
+              <button className="mx-2 btn btn-primary" type="Submit">Add</button>
+            </form>
+            <br/>
+            <div>
+              {this.state.taskList.map((task)=> 
+                <div className="my-2 border rounded border-primary"style={style1}>
+                  <p style={style2}>{task.task}</p>
+                  <button className="btn btn-success mb-1 ml-5" style={style2} onClick={() => this.deleteTask(task.id)}>done</button>
+                </div>
+              )}
+            </div>
+ 
+          </div>
+      )
+  }
+}
+
+const head = {
+  padding : "10px",
+  backgroundColor : "white",
+  color: "blue"
+}
+
+const style1 = {
+  padding : "10px",
+  textAlign : "center",
+}
+
+const style2 = {
+  display: "inline",
+  marginLeft: "10px"
 }
 
 export default App;
